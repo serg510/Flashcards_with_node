c59c500062f5094e5753ca03d60f4d48624f32b8
sergio <sbarajas510@gmail.com>
2020-06-02 14:48:34 -0700

    added a route to get random cards && to take to specfic question if specific "card" is selected

--- routes/cards.js
+++ routes/cards.js
@@ -3,14 +3,32 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+//get random cards
+router.get('/',(req,res) => {
+    const numberOfCards = cards.length;
+    const flashcardId = Math.floor(Math.random() * numberOfCards );
+    res.redirect(`/cards/${flashcardId}?side=question`);
+})
+
 router.get('/:id',(req,res) => {
-    const { side } = req.query;
+    let { side } = req.query;
     const { id } = req.params;
+// if Question is selected in url take to that ${card}
+    if(!side){
+        res.redirect(`/cards/${id}?side=question`);
+    }
     const text = cards[id][side];
     const { hint } = cards[id];
-    const templateData = { text }
+
+    const templateData = { id , text }
+
     if(side === 'question'){
         templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+    } else if (side === 'answer'){
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
     }
     
     res.render('card', templateData);
